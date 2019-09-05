009af140f23654ca6969c1621107a7d451a0de04
lalitsingh <lalitbisalpur@gmail.com>
2019-09-05 14:08:49 +0530

    first

--- app/src/main/java/com/blagro/utilities/LocationTrack.java
+++ app/src/main/java/com/blagro/utilities/LocationTrack.java
@@ -6,28 +6,23 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
-import com.blagro.activity.MainActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
+/**
+ * Created by anupamchugh on 28/11/16.
+ */
 
 public class LocationTrack extends Service implements LocationListener {
 
-    Context mContext;
+    private final Context mContext;
 
 
     boolean checkGPS = false;
@@ -40,21 +35,13 @@ public class LocationTrack extends Service implements LocationListener {
     Location loc;
     double latitude;
     double longitude;
-    MainActivity.GetLocation getLocation;
-    String provider;
-    Timer timer;
-    TimerTask timerTask;
 
 
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
 
 
-    private static final long MIN_TIME_BW_UPDATES = 20000;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
     protected LocationManager locationManager;
-
-
-    public LocationTrack() {
-    }
 
     public LocationTrack(Context mContext) {
         this.mContext = mContext;
@@ -64,67 +51,49 @@ public class LocationTrack extends Service implements LocationListener {
     private Location getLocation() {
 
         try {
-            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
 
-            Criteria criteria = new Criteria();
-            provider = locationManager.getBestProvider(criteria, false);
+            // get GPS status
+            checkGPS = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
 
+            // get network provider status
+            checkNetwork = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if (provider != null && !provider.equals(" ")) {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-//                    return TODO;
-                }
-                loc = locationManager.getLastKnownLocation(provider);
+            if (!checkGPS && !checkNetwork) {
+                Toast.makeText(mContext, "No Service Provider is available", Toast.LENGTH_SHORT).show();
+            } else {
+                this.canGetLocation = true;
 
-                locationManager.requestLocationUpdates(provider, 20000, 1, this);
+                // if GPS Enabled get lat/long using GPS Services
+                if (checkGPS) {
 
-                // get GPS status
-                checkGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-                // get network provider status
-                checkNetwork = locationManager
-                        .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-                if (!checkGPS && !checkNetwork) {
-                    Toast.makeText(mContext, "No Service Provider is available", Toast.LENGTH_SHORT).show();
-                } else {
-                    this.canGetLocation = true;
-
-                    // if GPS Enabled get lat/long using GPS Services
-                    if (checkGPS) {
-
-                        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                        }
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                MIN_TIME_BW_UPDATES,
-                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        if (locationManager != null) {
-                            loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            locationManager.requestLocationUpdates(provider, 20000, 1, this);
-                            if (loc != null) {
-                                latitude = loc.getLatitude();
-                                longitude = loc.getLongitude();
-                                onLocationChanged(loc);
-                            }
-                        }
-
-
+                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
                     }
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        loc = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (loc != null) {
+                            latitude = loc.getLatitude();
+                            longitude = loc.getLongitude();
+                        }
+                    }
+
+
+                }
 
 
                 /*if (checkNetwork) {
@@ -156,7 +125,6 @@ public class LocationTrack extends Service implements LocationListener {
                     }
                 }*/
 
-                }
             }
 
 
@@ -209,7 +177,7 @@ public class LocationTrack extends Service implements LocationListener {
         });
 
 
-//        alertDialog.show();
+        alertDialog.show();
     }
 
 
@@ -231,40 +199,13 @@ public class LocationTrack extends Service implements LocationListener {
     }
 
     @Override
-    public void onCreate() {
-//        Handler handler=new Handler(Looper.myLooper());
-        super.onCreate();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        timerTask.run();
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onDestroy() {
-        stopListener();
-        timer.cancel();
-        super.onDestroy();
-    }
-
-    @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
     @Override
-    public void onLocationChanged(final Location location) {
-         timer=new Timer();
-         timerTask=new TimerTask() {
-            @Override
-            public void run() {
-                location.getLongitude();
-                location.getLongitude();
-            }
-        };
-        timer.schedule(timerTask, 20000,20000);
+    public void onLocationChanged(Location location) {
+
     }
 
     @Override
